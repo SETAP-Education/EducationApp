36b2181788709957e3da714470f507714faf1692
Henry Gronow <henryrgronow@gmail.com>
2024-02-02 11:07:05 +0000

    Added Quiz queries for sharecodes

--- education_app/lib/Quizzes/quizManager.dart
+++ education_app/lib/Quizzes/quizManager.dart
@@ -1,31 +1,30 @@
-
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:education_app/Quizzes/quiz.dart';
 
-// Quizzes are stored under the quizzes collection in the database 
+// Quizzes are stored under the quizzes collection in the database
 
 class QuizManager {
-
-  // Searches for quizzes with the specified tags 
-  // Returns an empty list if none exist 
-  Future<List<Quiz>> getQuizzesWithTags(List<String> tags) { 
-
+  // Searches for quizzes with the specified tags
+  // Returns an empty list if none exist
+  Future<List<Quiz>> getQuizzesWithTags(List<String> tags) {
     return Future(() => List.empty());
   }
 
-  // Returns the quiz with the specified ID 
-  // Returns null if the quiz is not found 
+  // Returns the quiz with the specified ID
+  // Returns null if the quiz is not found
   Future<Quiz?> getQuizWithId(String id) async {
-
     var db = FirebaseFirestore.instance;
-    
-    // Grab the object with a converter
-    var quizRef = await db.collection("quizzes").doc(id)
-      .withConverter(fromFirestore: Quiz.fromFirestore, toFirestore: (Quiz quiz, _) => quiz.toFirestore())
-      .get();
 
-    // Test if the quiz exists 
+    // Grab the object with a converter
+    var quizRef = await db
+        .collection("quizzes")
+        .doc(id)
+        .withConverter(
+            fromFirestore: Quiz.fromFirestore,
+            toFirestore: (Quiz quiz, _) => quiz.toFirestore())
+        .get();
+
+    // Test if the quiz exists
     // If it doesn't return null
     if (!quizRef.exists) {
       return Future(() => null);
@@ -34,10 +33,28 @@ class QuizManager {
     return Future(() => quizRef.data());
   }
 
-  // Returns the quiz from the sharecode. 
-  // Returns null if the quiz does not exist 
-  Future<Quiz?> getQuizFromShareCode(String shareCode) {
+  // Returns the quiz from the sharecode.
+  // Returns null if the quiz does not exist
+  Future<Quiz?> getQuizFromShareCode(String shareCode) async {
+    var db = FirebaseFirestore.instance;
 
+    // This is a simple query
+    var quiz = await db
+        .collection("quizzes")
+        .where("shareCode", isEqualTo: shareCode)
+        .withConverter(
+            fromFirestore: Quiz.fromFirestore,
+            toFirestore: (Quiz quiz, _) => quiz.toFirestore())
+        .get();
+
+    if (quiz.docs.isNotEmpty) {
+      // We found a quiz with this sharecode
+
+      // It should only get 1 so return the first in the array
+      return quiz.docs[0].data();
+    }
+
+    // If we reach this point just return null
     return Future(() => null);
   }
 
@@ -46,12 +63,16 @@ class QuizManager {
   Future<QuizQuestion?> getQuizQuestionById(String id) async {
     var db = FirebaseFirestore.instance;
 
-    var questionRef = await db.collection("questions").doc(id)
-      .withConverter(fromFirestore: QuizQuestion.fromFirestore, toFirestore: (QuizQuestion q, _) => q.toFirestore())
-      .get();
+    var questionRef = await db
+        .collection("questions")
+        .doc(id)
+        .withConverter(
+            fromFirestore: QuizQuestion.fromFirestore,
+            toFirestore: (QuizQuestion q, _) => q.toFirestore())
+        .get();
 
     if (!questionRef.exists) {
-      return null; 
+      return null;
     }
 
     return questionRef.data();
@@ -61,12 +82,16 @@ class QuizManager {
   Future<List<QuizQuestion>> getQuizQuestionsByTags(List<String> tags) async {
     var db = FirebaseFirestore.instance;
 
-    var questionRef = await db.collection("questions").where("tags", arrayContainsAny: tags)
-      .withConverter(fromFirestore: QuizQuestion.fromFirestore, toFirestore: (QuizQuestion q, _) => q.toFirestore())
-      .get();
+    var questionRef = await db
+        .collection("questions")
+        .where("tags", arrayContainsAny: tags)
+        .withConverter(
+            fromFirestore: QuizQuestion.fromFirestore,
+            toFirestore: (QuizQuestion q, _) => q.toFirestore())
+        .get();
 
-    
-    return List.generate(questionRef.docs.length, (index) => questionRef.docs[index].data());
+    return List.generate(
+        questionRef.docs.length, (index) => questionRef.docs[index].data());
   }
 
   // Returns an empty list if no questions that match are found
@@ -74,20 +99,16 @@ class QuizManager {
   // This returns all quiz questions in the database
   // AND IS SLOW
   Future<List<QuizQuestion>> getQuizQuestions() async {
-
     var db = FirebaseFirestore.instance;
 
-    var questionRef = await db.collection("questions")
-      .withConverter(fromFirestore: QuizQuestion.fromFirestore, toFirestore: (QuizQuestion q, _) => q.toFirestore())
-      .get();
+    var questionRef = await db
+        .collection("questions")
+        .withConverter(
+            fromFirestore: QuizQuestion.fromFirestore,
+            toFirestore: (QuizQuestion q, _) => q.toFirestore())
+        .get();
 
-    List<QuizQuestion> questions = List.empty(growable: true); 
-
-    for (var i in questionRef.docs) {
-      questions.add(i.data());
-    }
-
-    return questions;
+    return List.generate(
+        questionRef.docs.length, (index) => questionRef.docs[index].data());
   }
-  
 }
