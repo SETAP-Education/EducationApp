f2dd18237687ad28cfe970db79bddcd5d7b1d045
Rbuds123 <rbuds@outlook.com>
2024-04-16 18:35:02 +0100

    adds users interest and display name to database so can be used where ever they are needed

--- education_app/lib/Pages/AuthenticationPages/LoginPage.dart
+++ education_app/lib/Pages/AuthenticationPages/LoginPage.dart
@@ -221,7 +221,7 @@ class _LoginPageState extends State<LoginPage> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => DisplayUser(),
+                    builder: (context) => LandingPage(),
                   ),
                 );
               },
