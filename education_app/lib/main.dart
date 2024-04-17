8285d98dc957d29df7e2a317e04e09cd0c57d055
maxjewell99 <114839384+maxjewell99@users.noreply.github.com>
2024-04-17 10:26:43 +0100

    Home page UI is changed to match the "QuizPage" UI. Any other changes let me know.

--- education_app/lib/main.dart
+++ education_app/lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:education_app/Firebase/firebase_options.dart';
 import 'package:education_app/Pages/AuthenticationPages/LoginPage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
