3803bf8eb27b9b5b4104bee1fd47958dc50355b4
Alex Woodroof <alex@woodroof.co.uk>
2024-04-17 19:32:55 +0100

    Took back my bypass button + Added Other Topics Section (Users Interests section was looking a little bare) + Made them all work + Made it look better + Formatted the Display Name Page / Centralised + Changed Font Colour on splash screen and how the buttons were formatted. Wow this is a big commit.

--- education_app/lib/Pages/AuthenticationPages/DisplayNamePage.dart
+++ education_app/lib/Pages/AuthenticationPages/DisplayNamePage.dart
@@ -14,11 +14,13 @@ class _DisplayUser extends State<DisplayUser> {
   final TextEditingController _nameController = TextEditingController();
   User? _user;
   List<String> _selectedInterests = [];
+  List<String> _interestsList = [];
 
   @override
   void initState() {
     super.initState();
     _checkAuthState();
+    _fetchInterests();
   }
 
   void _checkAuthState() {
@@ -31,166 +33,177 @@ class _DisplayUser extends State<DisplayUser> {
     });
   }
 
+  void _fetchInterests() async {
+    try {
+      DocumentSnapshot interestsDoc = await FirebaseFirestore.instance
+          .collection('interests')
+          .doc('interests')
+          .get();
+
+      if (interestsDoc.exists) {
+        setState(() {
+          _interestsList = List<String>.from(interestsDoc['interests']);
+        });
+      }
+    } catch (error) {
+      print('Failed to fetch interests: $error');
+    }
+  }
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text('Display User'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () async {
-              // Sign out the user
-              await FirebaseAuth.instance.signOut();
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          LoginPage()));
-            },
-          ),
-        ],
-      ),
-      body: Row(
-        children: [
-          Container(
-            width: 250,
-            padding: EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Interests',
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      automaticallyImplyLeading: false,
+      actions: [
+        IconButton(
+          icon: Icon(Icons.logout),
+          onPressed: () async {
+            // Sign out the user
+            await FirebaseAuth.instance.signOut();
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => LoginPage()),
+            );
+          },
+        ),
+      ],
+    ),
+    body: Center(
+      child: Container(
+        width: 600,
+        padding: EdgeInsets.all(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                'Hey there! What should we call you?',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: Container(
+                width: 600,
+                child: TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                    labelText: 'Display Name',
+                    hintText: _user?.displayName,
+                    contentPadding: EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 15),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: secondaryColour),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: secondaryColour),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    labelStyle: TextStyle(color: secondaryColour),
+                  ),
                   style: GoogleFonts.nunito(
                     fontSize: 20.0,
                   ),
+                  cursorColor: secondaryColour,
+                  onEditingComplete: () {},
                 ),
-                SizedBox(height: 10),
-                GridView.builder(
-                  shrinkWrap: true,
-                  itemCount: _interestsList.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 8,
-                    mainAxisSpacing: 8,
-                  ),
-                  itemBuilder: (context, index) {
-                    final interest = _interestsList[index];
-                    return InkWell(
-                      onTap: () {
-                        setState(() {
-                          if (_selectedInterests.contains(interest)) {
-                            _selectedInterests.remove(interest);
-                          } else {
-                            _selectedInterests.add(interest);
-                          }
-                        });
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: _selectedInterests.contains(interest)
-                              ? Color(0xFF19c37d)
-                              : Colors.grey[300],
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        child: Center(
-                          child: Text(
-                            interest,
-                            style: TextStyle(
-                              color: _selectedInterests.contains(interest)
-                                  ? Colors.white
-                                  : Colors.black,
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                    child: Text(
-                      'Welcome, enter a display name!',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Center(
-                    child: Container(
-                      width: 600, // Set width to 600
-                      child: TextFormField(
-                        controller: _nameController,
-                        decoration: InputDecoration(
-                          labelText: 'Display Name',
-                          hintText: _user?.displayName,
-                          contentPadding: EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 15),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: secondaryColour),
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: secondaryColour),
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                          labelStyle: TextStyle(color: secondaryColour),
-                        ),
-                        style: GoogleFonts.nunito(
-                          fontSize: 20.0,
-                        ),
-                        cursorColor: secondaryColour,
-                        onEditingComplete: () {},
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Get the entered display name
-                        String displayName = _nameController.text.trim();
-
-                        if (displayName.isNotEmpty) {
-                          // Set the display name in Firebase database
-                          _setDisplayName(_user!.uid, displayName);
-
-                          // Save selected interests to Firestore
-                          _saveInterests(_user!.uid, _selectedInterests);
-
-                          // Navigate to the landing page
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => LandingPage()),
-                          );
-                        } else {
-                          // Show an error message or handle empty display name
-                          print('Display name cannot be empty');
-                        }
-                      },
-                      child: Text('Continue', style: GoogleFonts.nunito(color: Colors.black, fontSize: 17),
-
-                    ),
-                  )
-                  )],
               ),
             ),
-          ),
-        ],
+            SizedBox(height: 20),
+            Text(
+              'Interests',
+              style: GoogleFonts.nunito(
+                fontSize: 20.0,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              height: 300,
+              child: GridView.builder(
+                shrinkWrap: true,
+                itemCount: _interestsList.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  crossAxisSpacing: 1,
+                  mainAxisSpacing: 1,
+                ),
+                itemBuilder: (context, index) {
+                  final interest = _interestsList[index];
+                  return InkWell(
+                    onTap: () {
+                      setState(() {
+                        if (_selectedInterests.contains(interest)) {
+                          _selectedInterests.remove(interest);
+                        } else {
+                          _selectedInterests.add(interest);
+                        }
+                      });
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: _selectedInterests.contains(interest)
+                            ? Color(0xFF19c37d)
+                            : Colors.grey[300],
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Center(
+                        child: Text(
+                          interest,
+                          style: TextStyle(
+                            color: _selectedInterests.contains(interest)
+                                ? Colors.white
+                                : Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  // Get the entered display name
+                  String displayName = _nameController.text.trim();
+
+                  if (displayName.isNotEmpty) {
+                    // Set the display name in Firebase database
+                    _setDisplayName(_user!.uid, displayName);
+
+                    // Save selected interests to Firestore
+                    _saveInterests(_user!.uid, _selectedInterests);
+
+                    // Navigate to the landing page
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => LandingPage()),
+                    );
+                  } else {
+                    // Show an error message or handle empty display name
+                    print('Display name cannot be empty');
+                  }
+                },
+                child: Text('Continue',
+                    style: GoogleFonts.nunito(
+                        color: Colors.black, fontSize: 17)),
+              ),
+            )
+          ],
+        ),
       ),
-    );
-  }
+    ),
+  );
+}
+
+
+
 
   void _setDisplayName(String userId, String displayName) {
     final users = FirebaseFirestore.instance.collection('users');
@@ -217,13 +230,4 @@ class _DisplayUser extends State<DisplayUser> {
       print('Failed to save Interests: $error');
     });
   }
-
-  // List of interests/tags
-  final List<String> _interestsList = [
-    'Maths',
-    'Networks',
-    'Database',
-    'Security',
-    'Programming'
-  ];
 }
