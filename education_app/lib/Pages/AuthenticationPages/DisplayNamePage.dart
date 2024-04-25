eaad1b55519dfebff294c3fc8218e6054babd976
Alex Woodroof <alex@woodroof.co.uk>
2024-04-25 17:29:48 +0100

    Trying to implement erroring on the DisplayNamePage.dart not working...

--- education_app/lib/Pages/AuthenticationPages/DisplayNamePage.dart
+++ education_app/lib/Pages/AuthenticationPages/DisplayNamePage.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:education_app/Widgets/Button.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:education_app/Pages/LandingPage.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:education_app/Theme/AppTheme.dart';
+import 'package:education_app/Pages/AuthenticationPages/ErrorDisplayer.dart';
 
 class DisplayUser extends StatefulWidget {
   @override
@@ -77,166 +78,178 @@ class _DisplayUserState extends State<DisplayUser> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppTheme.buildAppBar(context, 'Quiz App', false, false, "Welcome to our quiz app", Text('')),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Container(
-            width: 600,
-            padding: EdgeInsets.all(8),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Center(
+              child: Container(
+                width: 600,
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Column(
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Text("Hey There!! ",
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text("Hey There!! ",
+                                  style: GoogleFonts.nunito(
+                                      fontSize: 38,
+                                      fontWeight: FontWeight.bold,
+                                      fontStyle: FontStyle.italic)),
+                              Text(
+                                "👋",
+                                style: TextStyle(fontSize: 38),
+                              )
+                            ],
+                          ),
+                          Text("What should we call you?",
                               style: GoogleFonts.nunito(
-                                  fontSize: 38,
-                                  fontWeight: FontWeight.bold,
-                                  fontStyle: FontStyle.italic)),
-                          Text(
-                            "👋",
-                            style: TextStyle(fontSize: 38),
-                          )
+                                  fontSize: 26, fontWeight: FontWeight.w600))
                         ],
                       ),
-                      Text("What should we call you?",
-                          style: GoogleFonts.nunito(
-                              fontSize: 26, fontWeight: FontWeight.w600))
-                    ],
-                  ),
-                ),
-                SizedBox(height: 20),
-                Center(
-                  child: Container(
-                    width: 600,
-                    child: TextFormField(
-                      controller: _nameController..text = _displayName,
-                      decoration: InputDecoration(
-                        labelText: 'Display Name',
-                        contentPadding:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              color: Theme.of(context).textTheme.bodyMedium!.color!),
-                          borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    SizedBox(height: 20),
+                    Center(
+                      child: Container(
+                        width: 600,
+                        child: TextFormField(
+                          controller: _nameController,
+                          decoration: InputDecoration(
+                            labelText: 'Display Name',
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Theme.of(context).textTheme.bodyMedium!.color!),
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                          ),
+                          style: GoogleFonts.nunito(fontSize: 20.0),
+                          cursorColor: Theme.of(context).textTheme.bodyMedium!.color!,
                         ),
                       ),
-                      style: GoogleFonts.nunito(fontSize: 20.0),
-                      cursorColor: Theme.of(context).textTheme.bodyMedium!.color!,
-                      onEditingComplete: () {},
                     ),
-                  ),
-                ),
-                SizedBox(height: 30),
-                Text(
-                  'What do you wanna learn?',
-                  style: GoogleFonts.nunito(
-                    fontSize: 26.0,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Text(
-                  'psst. Don\'t worry you can still access the others later!',
-                  style: GoogleFonts.nunito(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w600,
-                    fontStyle: FontStyle.italic,
-                  ),
-                ),
-                SizedBox(height: 16),
-                Container(
-                  width: 400,
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: _interestsList.length,
-                    itemBuilder: (context, index) {
-                      final interest = _interestsList[index];
-                      return Padding(
-                        padding: const EdgeInsets.only(bottom: 16.0),
-                        child: InkWell(
-                          splashColor: !_selectedInterests.contains(interest)
-                              ? Color(0xFF19c37d)
-                              : Colors.white,
-                          borderRadius: BorderRadius.circular(24),
-                          onTap: () {
-                            setState(() {
-                              if (_selectedInterests.contains(interest)) {
-                                _selectedInterests.remove(interest);
-                              } else {
-                                _selectedInterests.add(interest);
-                              }
-                            });
-                          },
-                          child: Ink(
-                            height: 94,
-                            padding: EdgeInsets.all(8),
-                            decoration: BoxDecoration(
-                              color: _selectedInterests.contains(interest)
+                    SizedBox(height: 30),
+                    Text(
+                      'What do you wanna learn?',
+                      style: GoogleFonts.nunito(
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'psst. Don\'t worry you can still access the others later!',
+                      style: GoogleFonts.nunito(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w600,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Container(
+                      width: 400,
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: _interestsList.length,
+                        itemBuilder: (context, index) {
+                          final interest = _interestsList[index];
+                          return Padding(
+                            padding: const EdgeInsets.only(bottom: 16.0),
+                            child: InkWell(
+                              splashColor: !_selectedInterests.contains(interest)
                                   ? Color(0xFF19c37d)
                                   : Colors.white,
                               borderRadius: BorderRadius.circular(24),
-                            ),
-                            child: Center(
-                              child: Text(
-                                interest,
-                                style: GoogleFonts.nunito(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
+                              onTap: () {
+                                setState(() {
+                                  if (_selectedInterests.contains(interest)) {
+                                    _selectedInterests.remove(interest);
+                                  } else {
+                                    _selectedInterests.add(interest);
+                                  }
+                                });
+                              },
+                              child: Ink(
+                                height: 94,
+                                padding: EdgeInsets.all(8),
+                                decoration: BoxDecoration(
                                   color: _selectedInterests.contains(interest)
-                                      ? Colors.white
-                                      : Colors.black,
+                                      ? Color(0xFF19c37d)
+                                      : Colors.white,
+                                  borderRadius: BorderRadius.circular(24),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    interest,
+                                    style: GoogleFonts.nunito(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: _selectedInterests.contains(interest)
+                                          ? Colors.white
+                                          : Colors.black,
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
+                          );
+                        },
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Center(
+                      child: Button(
+                        width: 400,
+                        important: true,
+                        onClick: () {
+                          // Get the entered display name
+                          String displayName = _nameController.text.trim();
+
+                          // Check if display name or interests are empty
+                          if (displayName.isEmpty) {
+                              // Add an error message to the error manager
+                              print("No display name");
+                              globalErrorManager.pushError('Display name cannot be empty');
+                          } else if (_selectedInterests.isEmpty) {
+                              // Add an error message to the error manager
+                              print("No interests");
+                              globalErrorManager.pushError('You must select at least one interest');
+                          } else {
+                              // If there are no errors, proceed with setting the display name and interests
+                              _setDisplayName(_user!.uid, displayName);
+                              _saveInterests(_user!.uid, _selectedInterests);
+                              // Navigate to the landing page
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => LandingPage()),
+                              );
+                          }
+                        },
+                        child: Text('Let\'s go!',
+                            style: GoogleFonts.nunito(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold)),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                  ],
                 ),
-                SizedBox(height: 20),
-                Center(
-                  child: Button(
-                    width: 400,
-                    important: true,
-                    onClick: () {
-                      // Get the entered display name
-                      String displayName = _nameController.text.trim();
-
-                      if (displayName.isNotEmpty) {
-                        // Set the display name in Firebase database
-                        _setDisplayName(_user!.uid, displayName);
-
-                        // Save selected interests to Firestore
-                        _saveInterests(_user!.uid, _selectedInterests);
-
-                        // Navigate to the landing page
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(builder: (context) => LandingPage()),
-                        );
-                      } else {
-                        // Show an error message or handle empty display name
-                        print('Display name cannot be empty');
-                      }
-                    },
-                    child: Text('Let\'s go!',
-                        style: GoogleFonts.nunito(
-                            color: Colors.white,
-                            fontSize: 18,fontWeight: FontWeight.bold)),
-              )),
-              SizedBox(height: 20),
-            ],
+              ),
+            ),
           ),
-        ),
+
+          // // Add ErrorDisplayer widget to display error messages
+          // ErrorDisplayer(),
+        ],
       ),
-    ));
+    );
   }
 
   void _setDisplayName(String userId, String displayName) {
