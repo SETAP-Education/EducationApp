a493d382955ac665b5d3f946570fa00ba74df92b
Rbuds123 <rbuds@outlook.com>
2024-05-01 14:47:20 +0100

    more tests

---
+++ education_app/test/quizManager_test.dart
@@ -0,0 +1,25 @@
+import 'package:test/test.dart';
+import 'package:education_app/Quizzes/quizManager.dart';
+
+void main() {
+  group('RecentQuiz', () {
+    test('Constructor initializes properties correctly', () {
+      RecentQuiz recentQuiz = RecentQuiz();
+
+      expect(recentQuiz.id, "");
+      expect(recentQuiz.name, "");
+      expect(recentQuiz.xpEarned, 0);
+    });
+
+    test('Properties can be set correctly', () {
+      RecentQuiz recentQuiz = RecentQuiz();
+      recentQuiz.id = "123";
+      recentQuiz.name = "Quiz 1";
+      recentQuiz.xpEarned = 50;
+
+      expect(recentQuiz.id, "123");
+      expect(recentQuiz.name, "Quiz 1");
+      expect(recentQuiz.xpEarned, 50);
+    });
+  });
+}
