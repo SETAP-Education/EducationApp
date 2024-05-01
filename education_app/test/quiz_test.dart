1da37469cb85840ac638e83908e7900e6aca72b0
Rbuds123 <rbuds@outlook.com>
2024-05-01 14:11:58 +0100

    quiz tests done

--- education_app/test/quiz_test.dart
+++ education_app/test/quiz_test.dart
@@ -1,6 +1,6 @@
 import 'package:test/test.dart';
 import 'package:education_app/Quizzes/quiz.dart';
-import 'package:education_app/Pages/QuizBuilder.dart';
+
 void main() {
   group('areListsEqual', () {
     test('Lists with same elements in the same order should return true', () {
@@ -45,14 +45,37 @@ void main() {
     });
   });
 
-  group('getQuizDifficulty', () {
-    test('Empty quiz should return 0 difficulty', () {
-      Quiz quiz = Quiz();
-      int difficulty = quiz.getQuizDifficulty();
-
-
-      expect(difficulty, equals(0));
+    test('Test questionTypeToString', () {
+      expect(questionTypeToString(QuestionType.multipleChoice), 'Multiple Choice');
+      expect(questionTypeToString(QuestionType.fillInTheBlank), 'Fill in the Blank');
+      expect(questionTypeToString(QuestionType.dragAndDrop), 'Drag & Drop');
+      expect(questionTypeToString(QuestionType.none), '');
     });
-
+  test('Test QuestionFillInTheBlank.fromMap', () {
+    // Test case 1: Valid map with values for correctAnswer and userResponse
+    Map<String, dynamic> validMap = {
+      "correctAnswer": "true",
+      "userResponse": "true",
+    };
+    var question = QuestionFillInTheBlank.fromMap(validMap);
+    expect(question.correctAnswer, "true");
+    expect(question.userResponse, "true");
   });
+    test('Test QuestionFillInTheBlank.fromMap', () {
+      Map<String, dynamic> mapWithCorrectAnswerOnly = {
+        "correctAnswer": "true",
+      };
+      var question = QuestionFillInTheBlank.fromMap(mapWithCorrectAnswerOnly);
+      expect(question.correctAnswer, "true");
+      expect(question.userResponse,
+          ""); // userResponse should default to empty string
+    });
+    // Test case 4: Map with neither correctAnswer nor userResponse
+    test('Test QuestionFillInTheBlank.fromMap', () {
+    Map<String, dynamic> mapWithNeither = {};
+    var question = QuestionFillInTheBlank.fromMap(mapWithNeither);
+    expect(question.correctAnswer, ""); // correctAnswer should default to empty string
+    expect(question.userResponse, ""); // userResponse should default to empty string
+  });
+
 }
